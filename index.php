8eccda557f5d2ad321048f4e1f15ed3142415ff8
bunlengEarb <tekdeiplerngkyol018@gmail.com>
2021-10-10 22:33:41 -0500

    change index

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     <body>
         <?php
             echo "Hi bunleng earb , this is your first online website in PHP";
-		print "\n Hello Mam , would you like to book appointment with me ?";
+		print "\r\n Hello Mam , would you like to book appointment with me ?";
         ?>
     </body>
 </html>
